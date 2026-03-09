37dd94a9cc8cdcdbc7673809f7cc83910fd16035
Christian <ChrisSchmi@users.noreply.github.com>
2026-03-09 15:33:47 +0100

    added power shell profile

---
+++ PowerShellProfile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+﻿# Terminal-Icons (lazy)
+if (Get-Command git -ErrorAction SilentlyContinue) {
+	# https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal
+	Import-Module -Name Terminal-Icons
+}
+
+# PSReadLine(lazy)
+if (Get-Command git -ErrorAction SilentlyContinue) {
+	# https://www.hanselman.com/blog/adding-predictive-intellisense-to-my-windows-terminal-powershell-prompt-with-psreadline
+	Import-Module PSReadLine
+}
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# posh-git nur wenn Git verfügbar (lazy)
+if (Get-Command git -ErrorAction SilentlyContinue) {
+    Import-Module posh-git
+}
+
+
+
